505dcd44453505de2f71ec5dc03bf112302a15dc
Michael Aaron Murphy <mmstickman@gmail.com>
2015-12-10 03:19:13 -0600

    Further Simplify Day 04

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -3,16 +3,14 @@ extern crate crypto;
 use crypto::digest::Digest;
 use crypto::md5::Md5;
 
-fn main() { println!("{}", get_answer("iwrupvqb")); }
+const KEY: &'static str = "iwrupvqb";
+
+/// Continually hashes keys until the first key succeeds the `hash_matched()` function.
+fn main() { println!("{}", (1..).skip_while(|x| !hash_matched(x)).take(1).next().unwrap()); }
 
 /// Computes the md5_hash of a string and returns true if it starts with six zeroes.
-fn hash_is_match(input: &str) -> bool {
+fn hash_matched(input: &usize) -> bool {
     let mut sh = Md5::new();
-    sh.input_str(input);
+    sh.input_str(&format!("{}{}", KEY, *input));
     sh.result_str().starts_with("000000")
-}
-
-/// Continually hashes keys until the first key succeeds the `check_hash()` function.
-fn get_answer(input: &str) -> usize {
-    (1..).skip_while(|x| !hash_is_match(&format!("{}{:05}", input, x))).take(1).next().unwrap()
 }
